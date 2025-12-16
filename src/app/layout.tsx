193240be1f26c20e1a55243cddde24c64d561038
ASG Kali <dev.vivek16@gmail.com>
2025-12-16 22:51:06 +0530

    old

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,50 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "SANSKRIT: NATURAL INTELLIGENCE",
-  description: "Phonetical - Neither Spiritual, Nor Scriptual",
+  title: "Sanskrit: Natural Intelligence | Phonetical - Neither Spiritual, Nor Scriptual",
+  description:
+    "Varṇamālā: Unity of Language (Script), Maths (Numbers), Colours (Varna) and Music (Raga)",
+
+  keywords: [
+    "Sanskrit language",
+    "Sanskrit phonetics",
+    "Natural intelligence",
+    "Cognitive science",
+    "Scientific Sanskrit",
+    "Linguistics",
+    "AI and Sanskrit",
+  ],
+
+  openGraph: {
+    title: "Sanskrit: Natural Intelligence | Phonetical - Neither Spiritual, Nor Scriptual ",
+    description:
+      "Varṇamālā: Unity of Language (Script), Maths (Numbers), Colours (Varna) and Music (Raga)",
+    url: "https://apratyaksh.com/",
+    siteName: "Sanskrit: Natural Intelligence",
+    images: [
+      {
+        url: "https://i.ibb.co/23xyRZX8/pngvarnamala.png",
+        width: 1200,
+        height: 630,
+        alt: "Sanskrit Varnamala – Natural Intelligence",
+        type: "image/png",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Sanskrit: Natural Intelligence | Phonetical - Neither Spiritual, Nor Scriptual ",
+    description:
+      "Varṇamālā: Unity of Language (Script), Maths (Numbers), Colours (Varna) and Music (Raga).",
+    images: ["https://i.ibb.co/23xyRZX8/pngvarnamala.png"],
+  },
+
+  metadataBase: new URL("https://apratyaksh.com/"),
 };
+
 
 export default function RootLayout({
   children,
